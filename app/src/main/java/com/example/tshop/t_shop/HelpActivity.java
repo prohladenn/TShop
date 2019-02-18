7c5145d690d5cb7202e20a7d3b38249cc3cde050
prohladenn <valershik39@gmail.com>
2019-02-19 01:58:56 +0300

    Logic Help Done

--- app/src/main/java/com/example/tshop/t_shop/HelpActivity.java
+++ app/src/main/java/com/example/tshop/t_shop/HelpActivity.java
@@ -1,25 +1,101 @@
 package com.example.tshop.t_shop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HelpActivity extends AppCompatActivity {
+
+    String textProblem = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        initialize();
+        buttonBackListener();
+        frameProblemListener();
+        frameSendListener();
     }
 
-    ImageView buttonBack = findViewById(R.id.button_help_back);
-    EditText textName = findViewById(R.id.edit_text_help_name);
-    EditText textNumber = findViewById(R.id.edit_text_help_number);
-    EditText textTheme = findViewById(R.id.edit_text_help_theme);
-    FrameLayout frameProblem = findViewById(R.id.button_help_description);
-    FrameLayout frameSend = findViewById(R.id.button_help_send);
+    ImageView buttonBack;
+    EditText textName;
+    EditText textNumber;
+    EditText textTheme;
+    TextView textViewProblem;
+    FrameLayout frameProblem;
+    FrameLayout frameSend;
 
+    void initialize() {
+        buttonBack = findViewById(R.id.button_help_back);
+        textName = findViewById(R.id.edit_text_help_name);
+        textNumber = findViewById(R.id.edit_text_help_number);
+        textTheme = findViewById(R.id.edit_text_help_theme);
+        textViewProblem = findViewById(R.id.edit_text_help_description);
+        frameProblem = findViewById(R.id.button_help_description);
+        frameSend = findViewById(R.id.button_help_send);
+    }
+
+    private void buttonBackListener() {
+        frameProblem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void frameProblemListener() {
+        frameProblem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpActivity.this, HelpDescriptionActivity.class);
+                if (!textProblem.isEmpty()) {
+                    intent.putExtra("text_problem", textProblem);
+                    textProblem = "";
+                } else intent.putExtra("text_problem", "");
+                startActivityForResult(intent, 1234);
+            }
+        });
+    }
+
+    private void frameSendListener() {
+        frameSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textProblem.isEmpty())
+                    Toast.makeText(HelpActivity.this, "Заполните отзыв!",
+                            Toast.LENGTH_LONG).show();
+                else Toast.makeText(HelpActivity.this, "Ваш отзыв отправлен!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1234 && resultCode == RESULT_OK && data != null) {
+
+            textProblem = data.getStringExtra("text_problem");
+            Log.d("RTR", textProblem);
+            if (textProblem.isEmpty())
+                textViewProblem.setText(R.string.help_description);
+            else
+                textViewProblem.setText(textProblem);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
--- app/src/main/java/com/example/tshop/t_shop/HelpDescriptionActivity.java
+++ app/src/main/java/com/example/tshop/t_shop/HelpDescriptionActivity.java
@@ -1,20 +1,65 @@
 package com.example.tshop.t_shop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 public class HelpDescriptionActivity extends AppCompatActivity {
 
+    String textProblem = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_description);
+        initialize();
+        textProblem = getIntent().getStringExtra("text_problem");
+        editTextProblem.setText(textProblem);
+        buttonBackListener();
+        buttonCheckListener();
     }
 
-    ImageView buttonBack = findViewById(R.id.button_help_desk_back);
-    ImageView buttonCheck = findViewById(R.id.button_help_desk_check);
-    EditText textProblem = findViewById(R.id.edit_text_help_problem);
+    ImageView buttonBack;
+    ImageView buttonCheck;
+    EditText editTextProblem;
 
+    void initialize() {
+        buttonBack = findViewById(R.id.button_help_desk_back);
+        buttonCheck = findViewById(R.id.button_help_desk_check);
+        editTextProblem = findViewById(R.id.edit_text_help_problem);
+    }
+
+    private void buttonBackListener() {
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void buttonCheckListener() {
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("text_problem", editTextProblem.getText().toString().trim());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!textProblem.isEmpty()) {
+            Intent intent = new Intent();
+            intent.putExtra("text_problem", textProblem);
+            setResult(RESULT_OK, intent);
+        }
+        finish();
+    }
 }
